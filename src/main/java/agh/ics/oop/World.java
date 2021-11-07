21436c7ccbef1ca03c67f7080919d50a636b9359
MaOlszewska <marto12@wp.pl>
2021-11-07 14:07:05 +0100

    finished lab3

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,37 +1,23 @@
 package agh.ics.oop;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 import static java.lang.System.out;
 
 public class World {
 
     public static void main(String[] args){
-    Vector2d position1 = new Vector2d(1,2);
-    out.println(position1);
-    Vector2d position2 = new Vector2d(-2,1);
-    out.println(position2);
-    out.println(position1.add(position2));
-    MapDirection s = MapDirection.SOUTH;
-    out.println(s.toUnitVector());
-    }
 
-    static void run(ArrayList<Direction> arguments){
-        for(Direction argument : arguments) {
-            switch (argument){
-                case FORWARD:
-                    out.println("Zwierzak idzie do przodu");
-                    break;
-                case BACKWARD:
-                    out.println("Zwierzak idzie do tyłu");
-                    break;
-                case RIGHT:
-                    out.println("Zwierzak skręca w prawo");
-                    break;
-                case LEFT:
-                    out.println("Zwierzak skręca w lewo");
-                    break;
-            }
-        }
+        Animal animal = new Animal();
+        out.println(animal.toString());
+        animal.move(MoveDirection.RIGHT);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        out.println(animal.toString());
+
+        OptionParser p = new OptionParser();
+        String[] moves = new String[]{"l","forward","kot","left","backward"};
+        for(MoveDirection m : p.parse(moves)){ animal.move(m);}
+        out.println(animal.toString());
     }
 }
-
---
+++ src/test/java/AnimalTest.java
@@ -0,0 +1,75 @@
+package agh.ics.oop;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnimalTest {
+
+    @Test
+    public void orientationTest(){
+        Animal a = new Animal();
+        String[] moves = new String[]{"l","forward","left","backward"};
+        OptionParser p = new OptionParser();
+        for(MoveDirection move : p.parse(moves)){a.move(move);}
+
+        assertEquals(a.getOrientation(), MapDirection.SOUTH );
+
+        String[] moves1 = new String[]{"r","forward", "b","b", "right"};
+        for(MoveDirection move : p.parse(moves1)){a.move(move);}
+
+        assertEquals(a.getOrientation(), MapDirection.NORTH);
+    }
+
+    @Test
+    public void positionTest(){
+        Animal a = new Animal();
+        String[] moves = new String[]{"l", "f", "left", "b"};
+        OptionParser p = new OptionParser();
+        for(MoveDirection move : p.parse(moves)){a.move(move);}
+
+        assertEquals(a.getPosition(), new Vector2d(1,3));
+
+        String[] moves1 = new String[]{"r","forward", "b","b", "right"};
+        for(MoveDirection move : p.parse(moves1)){a.move(move);}
+
+        assertEquals(a.getPosition(), new Vector2d(2,3));
+    }
+
+    @Test
+    public void mapTest(){
+        Animal a = new Animal();
+        String[] moves = new String[]{"f", "f", "f", "f", "f", "f", "f"};
+        OptionParser p = new OptionParser();
+        for(MoveDirection move : p.parse(moves)){a.move(move);}
+
+        assertEquals(a.getPosition(), new Vector2d(2,4));
+        assertEquals(a.getOrientation(),MapDirection.NORTH);
+
+        String[] moves1 = new String[]{"r", "f", "f", "f", "r", "f", "f", "f", "f", "f","f"};
+        for(MoveDirection move : p.parse(moves1)){a.move(move);}
+
+        assertEquals(a.getPosition(), new Vector2d(4,0));
+        assertEquals(a.getOrientation(),MapDirection.SOUTH);
+
+    }
+
+    @Test
+    public void interpretationTest(){
+        Animal a = new Animal();
+        String [] moves = new String[]{"f", "chomik", "right", "Kot", "left", "l", "pies", "backward", "Rybki"};
+        OptionParser p = new OptionParser();
+        for(MoveDirection move : p.parse(moves)){a.move(move);}
+
+        assertEquals(a.getPosition(),new Vector2d(3,3));
+        assertEquals(a.getOrientation(),MapDirection.WEST);
+
+        String [] moves1 = new String[]{"chomik", "Kot", "pies", "Rybki", "szynszyl"};
+        for(MoveDirection move : p.parse(moves1)){a.move(move);}
+
+        assertEquals(a.getPosition(),new Vector2d(3,3));
+        assertEquals(a.getOrientation(),MapDirection.WEST);
+
+
+    }
+}
