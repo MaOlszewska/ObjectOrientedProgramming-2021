65ac304f322ee5411b6bc4058e3f784cefd350b6
MaOlszewska <marto12@wp.pl>
2021-11-12 19:01:25 +0100

    lab4

--- src/main/java/agh/ics/oop/SimulationEngine.java
+++ src/main/java/agh/ics/oop/SimulationEngine.java
@@ -1,37 +1,36 @@
 package agh.ics.oop;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class SimulationEngine implements IEngine{
     private final MoveDirection[] directions;
     private final IWorldMap map;
+    private final Vector2d[] positions;
+    private final ArrayList<Animal> animals;
 
     public SimulationEngine(MoveDirection[] directions, IWorldMap map, Vector2d[] positions){
         this.directions = directions;
         this.map = map;
-        ArrayList<Animal> animals = new ArrayList<>();
+        this.positions = positions;
+        this.animals = new ArrayList<>();
 
         // dodaje zwierzaki do mapy
-        for(Vector2d position : positions){
-            Animal newAnimal = new Animal(map,position);
-            if (map.place(newAnimal)) {
-                map.place(new Animal(this.map,position));
+        for(Vector2d position : this.positions){
+            Animal newAnimal = new Animal(this.map,position);
+            if (this.map.place(newAnimal)) {
+                animals.add(newAnimal);
             }
         }
     }
 
     @Override
     public void run() {
-        RectangularMap map = (RectangularMap) this.map;
-        ArrayList<Animal> animals = map.getAnimals();
         int l = animals.size();
         int i = 0;
-
         for (MoveDirection direction: this.directions) {
             animals.get(i % l).move(direction);
             i ++;
-
         }
     }
 }
